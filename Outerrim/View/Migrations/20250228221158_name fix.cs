5cb77f87ba19821f82f8d8be5189b5e7114e9ac3
sayo <olafplazinski23@gmail.com>
2025-02-28 23:57:45 +0100

    added the missing files

---
+++ Outerrim/View/Migrations/20250228221158_name fix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace View.Migrations
+{
+    /// <inheritdoc />
+    public partial class namefix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StructureId",
+                table: "AircraftSpecifications",
+                newName: "Structure");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Structure",
+                table: "AircraftSpecifications",
+                newName: "StructureId");
+        }
+    }
+}
